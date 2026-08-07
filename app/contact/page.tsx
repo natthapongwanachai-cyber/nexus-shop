aa53dabe728112d33afcfbd3166d61ddad37cb6a
natthapongwanachai-cyber <natthapongwanachai@gmail.com>
2026-08-07 11:23:18 +0700

    add auto-generated pages for topup, cart, profile, contact, settings

---
+++ app/contact/page.tsx
@@ -0,0 +1,1 @@
+export default function Page() { return (<div className="flex min-h-screen flex-col items-center justify-center p-24 text-white bg-gray-900"><h1 className="text-4xl font-bold mb-4 uppercase">contact</h1></div>); }
---
+++ app/profile/page.tsx
@@ -0,0 +1,1 @@
+export default function Page() { return (<div className="flex min-h-screen flex-col items-center justify-center p-24 text-white bg-gray-900"><h1 className="text-4xl font-bold mb-4 uppercase">profile</h1></div>); }
